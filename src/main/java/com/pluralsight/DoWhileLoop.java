19509301e437d3dbff8dbcf00a10aaa153268ac3
Sthealth <AW@example.com>
2025-10-02 11:15:52 -0400

    add code to the different classes to demonstrate loop usage

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            i++;
+            System.out.println("I love Java");
+        } while (i < 5);
+    }
 }
--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 5) {
+            i++;
+            System.out.println("I love Java");
+        }
+    }
 }
